51158ad5f94627c4c57ed9e8fa06c76cb7245920
charlesdominique <charlesdominiquebacanto@gmail.com>
2024-11-20 11:12:53 +0800

    Fine Tune

--- dateFormatter/Program.cs
+++ dateFormatter/Program.cs
@@ -5,56 +5,63 @@ class Program
     static void Main(string[] args)
     {   
 
-        Console.WriteLine("***Date Formatter***");
+        Console.WriteLine("**********Date Formatter**********\n");//Header
         //Prompt the user to Enter the needed inputs;
-        Console.WriteLine("Please enter the Month from 1 to 12: ");
+        Console.Write("Please enter the Month from 1 to 12: ");
         int month = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Please enter the Date: ");
+        Console.Write("Please enter the Date: ");
         int date = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Please enter the Year: ");
+        Console.Write("Please enter the Year: ");
         int year = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("\n");//Create Space
+        Console.WriteLine("Month          : "+month);
+        Console.WriteLine("Date           : "+date);
+        Console.WriteLine("Year           : " + year);
+        Console.WriteLine("\n");//Create Space
+        Console.Write("Formatted Date : "); 
+
 
         //Switch Cases for each month
         switch (month)
         {
             case 1:
-                Console.WriteLine("January " + date + ", " + year);
+                Console.Write("January " + date + ", " + year);
                 break;
             case 2:
-                Console.WriteLine("February " + date + ", " + year);
+                Console.Write("February " + date + ", " + year);
                 break;
             case 3:
-                Console.WriteLine("March " + date + ", " + year);
+                Console.Write("March " + date + ", " + year);
                 break;
             case 4:
-                Console.WriteLine("April " + date + ", " + year);
+                Console.Write("April " + date + ", " + year);
                 break;
             case 5:
-                Console.WriteLine("May " + date + ", " + year);
+                Console.Write("May " + date + ", " + year);
                 break;
             case 6:
-                Console.WriteLine("June " + date + ", " + year);
+                Console.Write("June " + date + ", " + year);
                 break;
             case 7:
-                Console.WriteLine("July " + date + ", " + year);
+                Console.Write("July " + date + ", " + year);
                 break;
             case 8:
-                Console.WriteLine("August " + date + ", " + year);
+                Console.Write("August " + date + ", " + year);
                 break;
             case 9:
-                Console.WriteLine("September " + date + ", " + year);
+                Console.Write("September " + date + ", " + year);
                 break;
             case 10:
-                Console.WriteLine("October " + date + ", " + year);
+                Console.Write("October " + date + ", " + year);
                 break;
             case 11:
-                Console.WriteLine("November " + date + ", " + year);
+                Console.Write("November " + date + ", " + year);
                 break;
             case 12:
-                Console.WriteLine("December " + date + ", " + year);
+                Console.Write("December " + date + ", " + year);
                 break;
             default:
-                Console.WriteLine("Invalid Month Input"); // Print out for the numbers that are not months
+                Console.WriteLine("Invalid Month Input"); // Print out for the invalid month numbers
                 break;
         }
     }
